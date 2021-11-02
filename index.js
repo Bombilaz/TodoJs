f24567a0d068d285e57b34ff48eb6e658a4dc84f
Bombilaz <Retho@yandex.ru>
2021-11-02 19:09:52 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,53 @@
-let form = document.querySelector('.input');
-let input = document.querySelector('.setTodo');
-let todos = document.querySelector('.todos');
-let link = 'img/close.png';
+const form = document.querySelector('.input');
+const input = document.querySelector('.setTodo');
+const todos = document.querySelector('.todos');
+const link = 'img/close.png';
+
+
+const create = () =>{
+/* 
+   Создаем задачу(elemBlock) которая состоит из названия задачи
+   (rightElemOfBlock) и img иконки для удаления (leftElemOfBlock)
+   в виде переменных
+*/ const middleElemOfBlock = document.createElement('div');
+   const leftElemOfBlock = document.createElement('div');
+   const rightElemOfBlock = document.createElement('img');
+   const elemBlock = document.createElement('div');
+/*
+   Назначаем класс стилей главному блоку , класс иконке для удаления
+   и атрибут который содержит ссылку на иконку.
+*/
+   leftElemOfBlock.classList.add('nametask');
+   middleElemOfBlock.classList.add('undone');
+   elemBlock.classList.add('task');
+   leftElemOfBlock.setAttribute('maxlength', "25");
+   leftElemOfBlock.setAttribute('contenteditable', true);
+   
+   rightElemOfBlock.setAttribute('src', link);
+   rightElemOfBlock.onclick = function(){
+      elemBlock.remove();
+   }
+
+   leftElemOfBlock.innerHTML = input.value;
+   middleElemOfBlock.onclick = () =>{
+      middleElemOfBlock.classList.toggle('done');
+      leftElemOfBlock.classList.toggle('text_decoration');
+   }
+   
+   todos.prepend(elemBlock);
+   elemBlock.prepend(rightElemOfBlock);
+   elemBlock.prepend(middleElemOfBlock);
+   elemBlock.prepend(leftElemOfBlock);
+   input.value = '';
+}
 
 function createTask(event){
    event.preventDefault();
-   let leftElemOfBlock = document.createElement('input');
-   let rightElemOfBlock = document.createElement('img');
-   let elemBlock = document.createElement('div');
-   elemBlock.classList.add('task');
-   rightElemOfBlock.classList.add('delete');
-   rightElemOfBlock.setAttribute('src', link);
-   leftElemOfBlock.value = input.value;
-   todos.prepend(elemBlock);
-   elemBlock.prepend(rightElemOfBlock);
-   elemBlock.prepend(leftElemOfBlock);
-   input.value = '';
-   let elements = document.querySelectorAll('.task');
-   
-   let buttons = document.querySelectorAll('.delete');
-   for(let i = 0; i < elements.length; i++){
-      buttons[i].onclick = function(){
-        elements[i].remove();
-      }
+   const elements = document.querySelectorAll('.task');
+   if(elements.length > 7){
+      return;
    }
-   
+   create();
 }
 
 form.addEventListener('submit', createTask);
