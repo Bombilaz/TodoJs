cd88d18bb25958c87b1d2ddf0f9fc14a398dc4d6
Bombilaz <Retho@yandex.ru>
2021-11-13 20:55:36 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,143 +3,162 @@ const inputNewTaskName = document.querySelector('.createNameForNewTask');
 const divCountTaskName = document.querySelector('.currentLength');
 const divsMaxNameNewTask = document.querySelectorAll('.topAlertElements');
 const taskList = document.querySelector('.taskList');
+const taskListCompleted = document.querySelector('.taskListCompleted');
 const tasksLength = document.querySelector('.currentLengthTask');
 const divsMaxTasks = document.querySelectorAll('.bottomAlertElements');
 const buttonForAddTask = document.querySelector('.addNewTask');
 
-
-let countTaskValue = 0;
-let arrayOfTasks = [];
-
+const arrayOfTasks = JSON.parse(localStorage.getItem('arr')) || [];
 showCountTasks();
 setInterval(checkNewTaskNameLength, 30);
-
-
+if (arrayOfTasks) {
+   render();
+}
 formCreateTask.addEventListener('submit', createTask);
 
-function createTask(event){
+function createTask(event) {
    event.preventDefault();
-   if(countTaskValue > 7){
+   if (arrayOfTasks.length > 7) {
       makeBackgroundColor(buttonForAddTask, 'red');
-      makeRedColor(divsMaxTasks , null);
+      makeRedColor(divsMaxTasks, null);
       setTimeout(makeBackgroundColor, 500, buttonForAddTask, 'black');
-      setTimeout(makeGreyColor, 500 , divsMaxTasks);
+      setTimeout(makeGreyColor, 500, divsMaxTasks);
       return;
    }
-   create();
-   countTaskValue++;
+   if (inputNewTaskName.value.trim() == '') {
+      return;
+   }
+   pushNewElem();
+   inputNewTaskName.value = '';
+   render();
    showCountTasks();
 }
 
-function create(){
+function render() {
+   taskList.innerHTML = '';
+   arrayOfTasks.forEach((item) => {
+      const index = arrayOfTasks.indexOf(item);
+      const statusTaskElem = document.createElement('div');
+      const taskNameElem = document.createElement('span');
+      const buttonDeleteTask = document.createElement('div');
+      const taskBlock = document.createElement('div');
 
-   const statusTaskElem = document.createElement('div');
-   const taskNameElem = document.createElement('div');
-   const buttonDeleteTask = document.createElement('div');
-   const taskBlock = document.createElement('div');
-   
-   addClass(taskNameElem, 'nametask');
-   addClass(buttonDeleteTask, 'delete');
-   addClass(statusTaskElem, 'undone');
-   addClass(taskBlock , 'task');
+      addClass(taskNameElem, 'nametask');
+      addClass(buttonDeleteTask, 'delete');
+      addClass(statusTaskElem, 'undone');
+      addClass(taskBlock, 'task');
+      taskNameElem.innerHTML = item.name;
 
-   setAttributesTaskNameElem(taskNameElem);
-   deleteTask(buttonDeleteTask, taskBlock);
-   createTaskName(taskNameElem);
-   if(createTaskName(taskNameElem).trim() == ''){
-      countTaskValue--;
-      return;
-   }
-   makeDone(statusTaskElem, taskNameElem, taskBlock);
-   
-   
-   addElemToApp(taskList, taskBlock);
-   addElemToApp(taskBlock, buttonDeleteTask);
-   addElemToApp(taskBlock, statusTaskElem);
-   addElemToApp(taskBlock, taskNameElem);
-   pushNewElem();
-   inputNewTaskName.value = '';
+      taskBlock.setAttribute('id', index);
+      if (item.completed) {
+         addElemToApp(taskList, taskBlock);
+         addElemToApp(taskBlock, buttonDeleteTask);
+         addElemToApp(taskBlock, statusTaskElem);
+         addElemToApp(taskBlock, taskNameElem);
+         addClass(statusTaskElem, 'done');
+         addClass(taskNameElem, 'text_decoration');
+         moveDownTask(taskBlock);
+      }
+      else {
+         addElemToApp(taskList, taskBlock);
+         addElemToApp(taskBlock, buttonDeleteTask);
+         addElemToApp(taskBlock, statusTaskElem);
+         addElemToApp(taskBlock, taskNameElem);
+      }
+
+      setContentEditAble(taskNameElem);
+
+      taskNameElem.addEventListener("keydown", function (event) {
+         c = event.keyCode;
+         if (c === 13 || c === 27) {
+            taskNameElem.blur();
+         }
+      });
+
+      buttonDeleteTask.addEventListener('click', () => {
+         if (index == taskBlock.getAttribute('id')) {
+            arrayOfTasks.splice(index, 1);
+            localStorage.setItem('arr', JSON.stringify(arrayOfTasks));
+            render();
+            showCountTasks();
+         }
+      })
+
+      statusTaskElem.addEventListener('click', () => {
+         item.completed = !item.completed;
+         localStorage.setItem('arr', JSON.stringify(arrayOfTasks));
+         render();
+         showCountTasks();
+      })
+   });
 }
 
-function pushNewElem(){
+
+function pushNewElem() {
    arrayOfTasks.push(
       {
-         names : inputNewTaskName.value, 
-         status : 'undone',
-      })
+         name: inputNewTaskName.value,
+         completed: false,
+      });
+   localStorage.setItem('arr', JSON.stringify(arrayOfTasks));
 }
 
-function makeDone(statusElem , taskNameElem , taskBlock){
-   statusElem.onclick = ()=>{
-      if(statusElem.classList.contains('done')) {
-         return;
-      }
-      console.log(statusElem);
-      moveDownTask(taskBlock);
-      addClass(statusElem, 'done');
-      addClass(taskNameElem, 'text_decoration');
-   }
-}
-
-function deleteTask(clickElem, deleteElem){
-   clickElem.onclick = ()=>{
-      deleteElem.remove();
-      countTaskValue--;
-      showCountTasks();
-   }
-}
-
-function checkNewTaskNameLength(){
+/* Функция которая проверяет не превышает ли имя новой задачи порог 
+в 25 символов , если превышает , то блокирует дальнейшее введение 
+символов , и информирует пользователя красным цветом */
+function checkNewTaskNameLength() {
    divCountTaskName.innerHTML = inputNewTaskName.value.length;
-   if(inputNewTaskName.value.length > 24){
-      makeRedColor(divsMaxNameNewTask , inputNewTaskName);
+   if (inputNewTaskName.value.length > 24) {
+      makeRedColor(divsMaxNameNewTask, inputNewTaskName);
       return;
    }
-   makeGreyColor(divsMaxNameNewTask , inputNewTaskName);
+   makeGreyColor(divsMaxNameNewTask, inputNewTaskName);
 }
 
-function moveDownTask(element){
+function moveDownTask(element) {
    taskList.appendChild(element);
 }
 
-function addElemToApp(mainElem, prepend){
+function addElemToApp(mainElem, prepend) {
    mainElem.prepend(prepend);
 }
 
-function createTaskName(element){
+function createTaskName(element) {
    return element.innerHTML = inputNewTaskName.value;
 }
 
-function showCountTasks(){
-   tasksLength.innerHTML = countTaskValue;
+function showCountTasks() {
+   let count = arrayOfTasks.length;
+   tasksLength.innerHTML = count;
 }
 
-function setAttributesTaskNameElem(variable){
+function setContentEditAble(variable) {
    variable.setAttribute('contenteditable', true);
    variable.setAttribute('onkeypress', "return (this.innerText.length <= 24)");
 }
 
-function addClass(elem , className){
+function addClass(elem, className) {
    elem.classList.add(className)
 }
 
-function makeBackgroundColor (elem, color){
+function makeBackgroundColor(elem, color) {
    elem.style.backgroundColor = color;
 }
 
-function makeRedColor (manyElems, one = null){
+function makeRedColor(manyElems, one = null) {
    manyElems.forEach(element => element.style.color = 'red');
-   if(one != null){
+   if (one != null) {
       one.style.color = 'red';
-   } 
+   }
 }
 
-function makeGreyColor (manyElems, one = null){
+function makeGreyColor(manyElems, one = null) {
    manyElems.forEach(element => element.style.color = 'rgba(0, 0, 0, 0.65)');
-   if(one != null){
+   if (one != null) {
       one.style.color = 'rgba(0, 0, 0, 0.65)';
    }
 }
+
 
 
 
